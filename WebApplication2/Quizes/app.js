2fc496177f424a524553997619e91dc4e651d448
ahmed Sarsour <ahmed.afef.sar@gmail.com>
2018-12-11 16:29:17 +0200

    changed a question spelling statement in quiz (only for check)

--- WebApplication2/Quizes/app.js
+++ WebApplication2/Quizes/app.js
@@ -42,7 +42,7 @@ function showScores() {
 
 var questions = [
     new Question("Which one is not an object oriented programming language?", ["java", "C#", "C++", "C"], "C"),
-    new Question("Which langue is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
     new Question("There are ____ main components of object oriented programming", ["1", "6", "2", "4"], "4"),
     new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
     new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework"),
