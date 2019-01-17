6049994b3fefe59d62d71a23e65eb72dceb2c7bf
Ori Hirshfeld <ori81510@gmail.com>
2019-01-17 05:33:35 -0800

    changed quiz 2, some other small changes

--- WebApplication2/Quizes/Quiz1/main.js
+++ WebApplication2/Quizes/Quiz1/main.js
@@ -2,24 +2,20 @@
 function check(){
 
 	var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
 	var question4 = document.quiz.question4.value;
 	var question5 = document.quiz.question5.value;
 	var correct = 0;
-	if (question1 == "soft landing") {
+	if (question1 == "5,10") {
 		correct++;
 	}
-	if (question2 == "yes") {
-		correct++;
-	}	
-	if (question3 == "Nintendo") {
+	if (question3 == "False") {
 		correct++;
 	}
-	if (question4 == "yes") {
+	if (question4 == "True") {
 		correct++;
 	}	
-	if (question5 == "coding") {
+	if (question5 == "All") {
 		correct++;
 	}
 	
