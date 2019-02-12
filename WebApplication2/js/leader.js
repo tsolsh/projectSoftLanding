b3b1f1b4a5414fe40c7e0c4b257f240dc9344b67
עדי צולשיין <ADI.TSOL1@GMAIL.COM>
2019-02-12 11:27:34 +0200

    nav+little of leader

---
+++ WebApplication2/js/leader.js
@@ -0,0 +1,30 @@
+﻿var c1 = document.getElementById("c1");
+var parent = document.getElementById("p1");
+c1.width = parent.offsetWidth - 40;
+c1.height = parent.offsetHeight - 40;
+
+var data1 = {
+    labels: ["chicago", "milwaukee", "green bay", "mackinac", "traverse city", "gary"],
+    datasets: [
+        {
+            fillColor: "rgba(255,255,255,.1)",
+            strokeColor: "rgba(255,255,255,1)",
+            pointColor: "#123",
+            pointStrokeColor: "rgba(255,255,255,1)",
+            data: [594, 614, 592, 734, 598, 599]
+        }
+    ]
+}
+
+var options1 = {
+    scaleFontColor: "rgba(255,255,255,1)",
+    scaleLineColor: "rgba(255,255,255,1)",
+    scaleGridLineColor: "transparent",
+    bezierCurve: false,
+    scaleOverride: true,
+    scaleSteps: 5,
+    scaleStepWidth: 50,
+    scaleStartValue: 500
+}
+
+new Chart(c1.getContext("2d")).Line(data1, options1)
--- WebApplication2/js/main.js
+++ WebApplication2/js/main.js
@@ -7,7 +7,7 @@
    [ Focus input ]*/
     $('.input100').each(function () {
         $(this).on('blur', function () {
-            if ($(this).val().trim() != "") {
+            if ($(this).val().trim() !== "") {
                 $(this).addClass('has-val');
             }
             else {
@@ -25,7 +25,7 @@
         var check = true;
 
         for (var i = 0; i < input.length; i++) {
-            if (validate(input[i]) == false) {
+            if (validate(input[i]) === false) {
                 showValidate(input[i]);
                 check = false;
             }
@@ -42,13 +42,13 @@
     });
 
     function validate(input) {
-        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        if ($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) === null) {
                 return false;
             }
         }
         else {
-            if ($(input).val().trim() == '') {
+            if ($(input).val().trim() === '') {
                 return false;
             }
         }
