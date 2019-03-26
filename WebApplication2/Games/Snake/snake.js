18a43204fe936259d7a6bb87ef47fa206baf88d3
עדי צולשיין <ADI.TSOL1@GMAIL.COM>
2019-03-26 23:52:47 +0200

    snake

--- WebApplication2/Games/Snake/snake.js
+++ WebApplication2/Games/Snake/snake.js
@@ -10,28 +10,26 @@ const box = 32;
 
 // load images
 
-const ground = new Image();
-ground.src = "img/ground.png";
+const ground = document.getElementById("ground");
 
-const foodImg = new Image();
-foodImg.src = "img/food.png";
+const foodImg = document.getElementById("food");
 
 // load audio files
 
-const dead = new Audio();
+/*const dead = new Audio();
 const eat = new Audio();
 const up = new Audio();
 const left = new Audio();
 const right = new Audio();
 const down = new Audio();
 
-dead.src = "audio/dead.mp3";
-eat.src = "audio/eat.mp3";
-up.src = "audio/up.mp3";
-left.src = "audio/left.mp3";
-right.src = "audio/right.mp3";
-down.src = "audio/down.mp3";
-
+dead.src = document.getElementById("dead");
+eat.src = document.getElementById("eat");
+up.src = document.getElementById("up");
+left.src = document.getElementById("left");
+right.src = document.getElementById("right");
+down.src = document.getElementById("down");
+*/
 
 // create the snake
 
@@ -62,19 +60,19 @@ let d;
 function direction(event) {
     if (event.keyCode === 37 && d !== "RIGHT") {
         d = "LEFT";
-        left.play();
+       // left.play();
     }
     else if (event.keyCode === 38 && d !== "DOWN") {
         d = "UP";
-        up.play();
+        //up.play();
     }
     else if (event.keyCode === 39 && d !== "LEFT") {
         d = "RIGHT";
-        right.play();
+        //right.play();
     }
     else if (event.keyCode === 40 && d !== "UP") {
         d = "DOWN";
-        down.play();
+        //down.play();
     }
 }
 
@@ -117,7 +115,7 @@ function draw() {
     // if the snake eats the food
     if (snakeX === food.x && snakeY === food.y) {
         score++;
-        eat.play();
+        //eat.play();
         food = {
             x: Math.floor(Math.random() * 17 + 1) * box,
             y: Math.floor(Math.random() * 15 + 3) * box
@@ -139,7 +137,7 @@ function draw() {
     if (snakeX < box || snakeX > 17 * box || snakeY < 3 * box
         || snakeY > 17 * box || collision(newHead, snake)) {
         clearInterval(game);
-        dead.play();
+        //dead.play();
         // show congratulations modal
         modal.classList.add("show");
         //closeicon on modal
