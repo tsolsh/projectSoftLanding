a7c6329bb289dda0e587229aaea261a183f412d8
Ori Hirshfeld <ori81510@gmail.com>
2019-05-30 21:44:16 +0300

    fixed compliation issue

--- WebApplication2/Games/completeTheCode/Properties/AssemblyInfo.cs
+++ WebApplication2/Games/completeTheCode/Properties/AssemblyInfo.cs
@@ -5,22 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("completeTheCode")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("completeTheCode")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dba9469f-b120-4b94-9b87-839bdd25fe81")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +21,3 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
